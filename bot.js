cafcdbc305da146a65b037e0821b2f22be522fc8
Jonas Winklmann <jonas.winklmann@web.de>
2018-07-18 01:25:34 +0200

    mrgl + saving people

--- bot.js
+++ bot.js
@@ -9,7 +9,11 @@ const fs = require('fs');
 
 const dataDir = 'data/'
 const memberDataFile = `${dataDir}memberdata.json`
+const moveDataFile = `${dataDir}moveData.json`
+var bot = new Discord.Client();
+var isReady = true
 
+bot.login("NDY4ODM2Njg0MjM1Mjc2Mjk5.DjAFew.xRSGOaL8dhM2k11jcjLYPM2P82I")
 
 function printTimes(channel) {
     let msg = ""
@@ -30,14 +34,13 @@ memberDataContainer.loadMemberData()
 
 // Initialize Discord client
 const client = new Discord.Client()
-
 client.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.content.substring(0, 1) == '!') {
         var args = message.content.substring(1).split(' ')
         var cmd = args[0]
-
+    
         args = args.splice(1)
         switch (cmd) {
             // !ping
@@ -49,8 +52,103 @@ client.on('message', message => {
             memberDataContainer.updateTimes()
             printTimes(message.channel)
             break;
+        case 'safeMeFrom':
+            fs.readFile(moveDataFile, function(err,content) {
+                if(err) {
+                    logger.info(`${err} occured while loading move data.`)
+                    let newSaved = message.mentions.members.first().id
+                    let data = {}
+                    data[newSaved] = []
+                    data[newSaved].push(message.author.id)
+                    const jsonStr = JSON.stringify(data)
+                    fs.writeFile (moveDataFile, jsonStr, function(err) {
+                        if (err) {
+                            logger.error(`${err} occured while saving move data.`)
+                        }
+                    })
+                } else {
+                    try {
+                        let newSaved = message.mentions.members.first().id
+                        logger.info(`${message.author.id} safed from ${newSaved}`)
+                        let data = JSON.parse(content)
+                        let containsKey = false
+                        for (let [key, toMove] of Object.entries(data)) {
+                            if(key == newSaved) {
+                                containsKey = true
+                                let contained = false
+                                for(id of toMove) {
+                                    if(id == message.author.id) {
+                                        contained = true
+                                    }
+                                }
+                                if(!contained) {
+                                    toMove.push(message.author.id)
+                                    const jsonStr = JSON.stringify(data)
+                                    fs.writeFile (moveDataFile, jsonStr, function(err) {
+                                        if (err) {
+                                            logger.error(`${err} occured while saving move data.`)
+                                        }
+                                    })
+                                }
+                            }
+                        }
+                        if(!containsKey) {
+                            data[newSaved] = []
+                            data[newSaved].push(message.author.id)
+                            const jsonStr = JSON.stringify(data)
+                            fs.writeFile (moveDataFile, jsonStr, function(err) {
+                                if (err) {
+                                    logger.error(`${err} occured while saving move data.`)
+                                }
+                            })
+                        }
+                    } catch (err) {
+                        logger.error(`${err} occured while loading member data.`)        
+                    }
+                }
+            })
+            message.delete();
+            break;
+        case 'sacrificeMeFor':
+            fs.readFile(moveDataFile, function(err,content) {
+                if (err) {
+                    logger.error(`${err} occured while saving move data.`)
+                } else {
+                    try {
+                        let data = JSON.parse(content)
+                        let newSaved = message.mentions.members.first().id
+                        data[newSaved].splice(data[newSaved].indexOf(message.author.id), 1)
+                        const jsonStr = JSON.stringify(data)
+                        fs.writeFile (moveDataFile, jsonStr, function(err) {
+                            if (err) {
+                                logger.error(`${err} occured while saving move data.`)
+                            }
+                        })
+                    } catch (err) {
+                        logger.error(`${err} occured while loading member data.`)        
+                    }
+                }
+            })
+            message.delete()
+            break
+        case 'mrgl':
+            if(isReady) {
+                isReady = false
+                let voiceChannel = message.member.voiceChannel
+                voiceChannel.join().then(connection => {
+                    console.debug("Joined channel")
+                    const dispatcher = connection.playFile(`${dataDir}murloc.mp3`)
+                    dispatcher.on("end", end => {
+                        voiceChannel.leave()
+                        isReady = true
+                    })
+                }).catch(err => logger.error(`${err} occured while saving move data.`))
+            }
+            message.delete();
+            break;
         }
     }
+    
 });
 
 client.on('presenceUpdate', (oldMember, newMember) => {
@@ -76,6 +174,26 @@ client.on('presenceUpdate', (oldMember, newMember) => {
             memberDataContainer.data[name] = userdata
         }
         memberDataContainer.saveMemberData()
+    }
+    if(status == 'online') {
+        fs.readFile(moveDataFile, function(err,content) {
+            if (err) {
+                logger.error(`${err} occured while saving move data.`)
+            } else {
+                try {
+                    let data = JSON.parse(content)
+                    for (let [key, toMove] of Object.entries(data)) {
+                        if(key == newMember.id) {
+                            for(idT of toMove) {
+                                client.guilds.get("259460880742350848").members.find("id", idT).setVoiceChannel(client.channels.find("id", "431931044913086474"))
+                            }
+                        }
+                    }
+                } catch (err) {
+                    logger.error(`${err} occured while loading member data.`)        
+                }
+            }
+        })
     }
 })
 
